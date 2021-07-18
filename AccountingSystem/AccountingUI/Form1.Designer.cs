14be10d9bf6f965bb4a9c080703091402a261f41
Xing <x.xing.unsw@gmail.com>
2021-07-18 22:47:06 +1000

    ReadExcel

--- AccountingSystem/AccountingUI/Form1.Designer.cs
+++ AccountingSystem/AccountingUI/Form1.Designer.cs
@@ -31,6 +31,7 @@ namespace AccountingUI
 		{
 			this.btnLoadExcel = new System.Windows.Forms.Button();
 			this.txtExcelFile = new System.Windows.Forms.TextBox();
+			this.button1 = new System.Windows.Forms.Button();
 			this.SuspendLayout();
 			// 
 			// btnLoadExcel
@@ -51,11 +52,22 @@ namespace AccountingUI
 			this.txtExcelFile.Size = new System.Drawing.Size(100, 23);
 			this.txtExcelFile.TabIndex = 1;
 			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(305, 46);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(75, 23);
+			this.button1.TabIndex = 2;
+			this.button1.Text = "button1";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
 			// Form1
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.button1);
 			this.Controls.Add(this.txtExcelFile);
 			this.Controls.Add(this.btnLoadExcel);
 			this.Name = "Form1";
@@ -69,6 +81,7 @@ namespace AccountingUI
 
 		private System.Windows.Forms.Button btnLoadExcel;
 		private System.Windows.Forms.TextBox txtExcelFile;
+		private System.Windows.Forms.Button button1;
 	}
 }
 
--- AccountingSystem/AccountingUI/Form1.cs
+++ AccountingSystem/AccountingUI/Form1.cs
@@ -1,11 +1,16 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Windows.Forms;
+using AccountingDatabase.Entity;
 using AccountingHelper.Helper;
 using AccountingHelper.Interface;
 using NLog;
 using NLog.Fluent;
+using NPOI.HSSF.UserModel;
 using NPOI.SS.UserModel;
+using NPOI.SS.Util;
 using NPOI.XSSF.UserModel;
 
 namespace AccountingUI
@@ -15,6 +20,8 @@ namespace AccountingUI
 		private readonly Logger _logger = LogManager.GetCurrentClassLogger();
 		private readonly IExcelHelper _excelHelper = new ExcelHelper();
 		private string path;
+
+		
 		public Form1()
 		{
 			InitializeComponent();
@@ -22,12 +29,119 @@ namespace AccountingUI
 
 		private void btnLoadExcel_Click(object sender, EventArgs e)
 		{
-			_logger.Error("Error");
 			var file = new OpenFileDialog();
 			file.ShowDialog();
 			this.txtExcelFile.Text = file.SafeFileName;
 			this.path = file.FileName;
 		}
 
+		private void button1_Click(object sender, EventArgs e)
+		{
+			GetTransactions(@"C:\Users\61425\Desktop\Data - Copy.xls");
+		}
+
+		private List<Transaction> GetTransactions(string filePath)
+		{
+			var transactions = new List<Transaction>();
+			var workbook = _excelHelper.ReadExcel(filePath);
+			if (workbook != null)
+			{
+				using var sheetReader = workbook.GetEnumerator();
+				while (sheetReader.MoveNext())
+				{
+					var sheet = sheetReader.Current;
+					if (sheet != null)
+					{
+						var reader = sheet.GetEnumerator();
+						var glCode = string.Empty;
+						var glDescription = string.Empty;
+						var year = string.Empty;
+						while (reader.MoveNext())
+						{
+							var row = reader.Current as HSSFRow;
+							if (row != null)
+							{
+								if(row.Cells.Count == 0 || row.Cells[0].Address.Column != 0 || row.Cells[0].Address.Column == 0 && row.Cells[0].CellType == CellType.Blank)
+									continue;
+								if (row.Cells.Count > 0 && row.Cells[0].CellType == CellType.String)
+								{
+									if (row.Cells[0].Address.Column == 0 && IsGLCode(row.Cells[0].StringCellValue))
+									{
+										glCode = row.Cells[0].StringCellValue;
+										glDescription = row.Cells[3].StringCellValue;
+										continue;
+									}
+
+									if (row.Cells[0].Address.Column == 0 && row.Cells[0].StringCellValue.Length == 4 && row.Cells[0].StringCellValue.StartsWith("20"))
+									{
+										year = row.Cells[0].StringCellValue;
+										continue;
+									}
+								}
+
+								var trans = new Transaction{GLCode = glCode, GLDescription = glDescription};
+								foreach (var cell in row.Cells)
+								{
+									if(cell.CellType == CellType.Blank)
+										continue;
+									switch (cell.Address.Column)
+									{
+										case 0:
+											trans.TransactionDate = new DateTime(int.Parse(year), int.Parse(cell.StringCellValue), 1);
+											break;
+										case 1:
+											trans.Source = new Source {Code = cell.StringCellValue};
+											break;
+										case 2:
+											trans.DocDate = cell.DateCellValue;
+											break;
+										case 3:
+											trans.Description = cell.StringCellValue;
+											break;
+										case 4:
+											if(cell.CellType == CellType.String && !cell.StringCellValue.StartsWith("Net Change and Ending Balance for Fiscal Period"))
+												trans.Description += $"*{cell.StringCellValue}";
+											break;
+										case 5:
+											trans.PostingSeq = cell.NumericCellValue.ToString();
+											break;
+										case 6:
+											trans.BatchEntry = cell.StringCellValue;
+											break;
+										case 7:
+											trans.Debit = (decimal) cell.NumericCellValue;
+											break;
+										case 8:
+											trans.Credit = (decimal) cell.NumericCellValue;
+											break;
+									}
+								}
+								transactions.Add(trans);
+							}
+						}
+					}
+				}
+			}
+
+			return transactions;
+		}
+
+		private bool IsGLCode(string code)
+		{
+			var parts = code.Split("-");
+			if (parts.Length == 1 && parts[0].Length == 6 && parts[0].All(x => char.IsDigit(x)))
+				return true;
+			if (parts.Length == 3)
+			{
+				if (parts[0].Length != 6 || parts[0].Any(x => !char.IsDigit(x)))
+					return false;
+				if (parts[1].Length != 4 || parts[1].Any(x => !char.IsLetter(x)))
+					return false;
+				if (parts[2].Length != 4 || parts[2].Any(x => !char.IsLetter(x)))
+					return false;
+				return true;
+			}
+			return false;
+		}
 	}
 }
