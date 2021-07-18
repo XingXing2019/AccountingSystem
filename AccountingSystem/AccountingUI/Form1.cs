bebd33d5afbff729e9fd9ea967ddc3ee763ced13
Xing <x.xing.unsw@gmail.com>
2021-07-18 18:19:06 +1000

    RemoveLog

--- AccountingSystem/AccountingUI/Form1.cs
+++ AccountingSystem/AccountingUI/Form1.cs
@@ -3,6 +3,8 @@ using System.IO;
 using System.Windows.Forms;
 using AccountingHelper.Helper;
 using AccountingHelper.Interface;
+using NLog;
+using NLog.Fluent;
 using NPOI.SS.UserModel;
 using NPOI.XSSF.UserModel;
 
@@ -10,6 +12,7 @@ namespace AccountingUI
 {
 	public partial class Form1 : Form
 	{
+		private readonly Logger _logger = LogManager.GetCurrentClassLogger();
 		private readonly IExcelHelper _excelHelper = new ExcelHelper();
 		private string path;
 		public Form1()
@@ -19,6 +22,7 @@ namespace AccountingUI
 
 		private void btnLoadExcel_Click(object sender, EventArgs e)
 		{
+			_logger.Error("Error");
 			var file = new OpenFileDialog();
 			file.ShowDialog();
 			this.txtExcelFile.Text = file.SafeFileName;
